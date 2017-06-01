66d4fdbe6807a53aa25bb89b4b15ce737bc96b9a
Trevor Pellegrini <trevorpellegrini@gmail.com>
2017-06-01 11:31:07 -0700

    added bcryptjs

--- controllers/controllers-users.js
+++ controllers/controllers-users.js
@@ -1,6 +1,7 @@
 'use strict';
 var db;
 const MongoClient = require('mongodb').MongoClient;
+var bcrypt = require('bcryptjs');
 var exports = module.exports = {};
 
 MongoClient.connect('mongodb://trevor:something@ds115071.mlab.com:15071/keystroke', (err, database) => {
@@ -12,55 +13,64 @@ MongoClient.connect('mongodb://trevor:something@ds115071.mlab.com:15071/keystrok
 });
 
 exports.createUser = (req, res) => {
-    console.log('body', req.body);
-    db.collection('users').find({email: req.body.email}).toArray((err, results) => {
-        if (results.length === 0) {
-            db.collection('users').save({
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                password: req.body.password,
-                description: req.body.description,
-                image: req.body.image
-            }, (err,results) => {
-                if (err) {
-                    return console.log(err);
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(`${req.body.password}/\/`, salt, function(err, hash) {
+            if (err) {
+                console.log(err);
+            }
+            db.collection('users').find({email: req.body.email}).toArray((err, results) => {
+                if (results.length === 0) {
+                    db.collection('users').save({
+                        firstName: req.body.firstName,
+                        lastName: req.body.lastName,
+                        email: req.body.email,
+                        password: hash,
+                        description: req.body.description,
+                        image: req.body.image
+                    }, (err, results) => {
+                        if (err) {
+                            return console.log(err);
+                        }
+                        db.collection('users').find({email: req.body.email, password: hash}).toArray((err, results) => {
+                            if (err) {
+                                console.log(err);
+                            }
+                            res.json(results);
+                        });
+                    });
+                } else {
+                    res.json({user: true});
                 }
-                db.collection('users').find({email: req.body.email, password: req.body.password}).toArray((err, results) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                    res.json(results);
-                });
             });
-        } else {
-            res.json({user: true});
-        }
+        });
     });
 };
 
-exports.updateUser = (req,res) => {
-    db.collection('users').update(
-        {
-            email:req.body.email
-        },
-        {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            password: req.body.password,
-            description: req.body.description,
-            image: req.body.image
-        }
-    );
+exports.updateUser = (req, res) => {
+    db.collection('users').update({
+        email: req.body.email
+    }, {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+        description: req.body.description,
+        image: req.body.image
+    });
     res.json('updated');
 };
 
 exports.login = (req, res) => {
-    console.log('body', req.body);
-    db.collection('users').find({email: req.body.email, password: req.body.password}).toArray((err, results) => {
-        console.log(results);
-        res.json(results);
+    db.collection('users').find({email: req.body.email}).toArray((err, results) => {
+        bcrypt.compare(`${req.body.password}/\/`, results[0].password, function(err, response) {
+            if(err){
+                console.log(err);
+            }if(response){
+                res.json(results);
+            }else{
+                res.json([]);
+            }
+        });
     });
 };
 
