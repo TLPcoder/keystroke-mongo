601c33a23a5e7649a8309e43a3410e9b15bb2bfb
Trevor Pellegrini <trevorpellegrini@gmail.com>
2017-06-02 11:53:39 -0700

    heroku

--- src/components/login-container.js
+++ src/components/login-container.js
@@ -2,32 +2,48 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 
-const LoginContainer = props => {
-    function login(){
-        axios.post('http://localhost:8080/users/login',{
-            email: document.getElementById('login-email').value,
-            password: document.getElementById('login-password').value
-        }).then(data =>{
+export default class LoginContainer extends {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email:'',
+            password:''
+        };
+    }
+    login() {
+        axios.post('http://localhost:8080/users/login', {
+            email: this.state.email,
+            password: this.state.password
+        }).then(data => {
             console.log(data);
-            if(data.data.length !== 0){
+            if (data.data.length !== 0) {
                 sessionStorage.setItem('email', data.data[0].email);
-                // props.history.push('/profile');
-            }else{
+                this.props.history.push('/profile');
+            } else {
                 alert('wrong email or password');
             }
         }).catch(err => {
             console.log(err);
         });
     }
-    return(
-        <div id = 'login-container'>
-            <input id = 'login-email' className='login-text' type="text" placeholder='email'/>
-            <br/>
-            <input id = 'login-password' className='login-text' type="password" placeholder='password'/>
-            <br/>
-            <input id='login-button' type="button" value='login' onClick ={login}/>
-        </div>
-    )
+    updateLogin(event){
+        if(event.target.id === 'login-email'){
+            this.state.email = event.target.value;
+        }else{
+            this.state.password = event.target.value;
+        }
+    }
+    render() {
+        return (
+            <div id='login-container'>
+                <input id='login-email' className='login-text' type="text" placeholder='email' onChange={this.updateLogin}/>
+                <br/>
+            <input id='login-password' className='login-text' type="password" placeholder='password' onChange={this.updateLogin}/>
+                <br/>
+                <input id='login-button' type="button" value='login' onClick ={login}/>
+            </div>
+        )
+    }
 }
-
+}
 export default LoginContainer;
